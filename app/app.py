e273fccfaf594f5b668209a6c5c2dbf84517a8d1
Padma Naresh <padmanaresh1986@gmail.com>
2024-03-15 22:57:13 +0530

    added health endpoint

--- app/app.py
+++ app/app.py
@@ -158,6 +158,9 @@ ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/health')
+def health():
+    return 'It is alive!\n'
 
 # API route for file upload and prediction
 @app.route('/upload', methods=['POST'])
